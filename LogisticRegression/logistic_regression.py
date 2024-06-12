36926f4dd9b8efc04223257008e9bd82c833bf92
Andy W <andy999coc@gmail.com>
2024-06-12 01:35:34 -0400

    successful impl of logistic reg

--- LogisticRegression/logistic_regression.py
+++ LogisticRegression/logistic_regression.py
@@ -1,33 +1,38 @@
 import numpy as np
+import scipy
 
 
 class LogisticRegression:
 
-    def __init__(self, iterations=100, learning_rate=1e-3, threshold=0.5):
-        self.iterations = iterations
+    def __init__(self, epochs=1000, learning_rate=1e-2, threshold=0.5):
+        self.epochs = epochs
         self.learning_rate = learning_rate
         self.threshold = threshold
         self.W = None  # weights
         self.b = None  # bias
         self.losses = []
 
-    def sigmoid(self, z):
+    def __sigmoid(self, z):
         # sigmoid activation function
-        return 1 / (1 + np.exp(-z))
 
-    def compute_loss(self, y, y_pred, epsilon=1e-9):
+        # return 1 / (1 + np.exp(-z))
+        return scipy.special.expit(z)  # handles np.exp(.) overflow
+
+    def __compute_loss(self, y, y_pred, epsilon=1e-9):
         # binary cross-entropy loss (BCE)
+
+        # epsilon added to prevent log(0)
         return -np.mean(y * np.log(y_pred + epsilon) + (1 - y) * np.log(1 - y_pred + epsilon))
 
     def fit(self, X, y):
         N, features = X.shape
-        self.W = np.random.randn(features)
-        self.b = 0
+        self.W = np.random.randn(features)  #
+        self.b = 0  # zero bias has no effect
 
-        for iter in range(self.iterations):
+        for epoch in range(self.epochs):
             z = np.matmul(X, self.W) + self.b
-            y_pred = self.sigmoid(z)
-            loss = self.compute_loss(y, y_pred)
+            y_pred = self.__sigmoid(z)
+            loss = self.__compute_loss(y, y_pred)
 
             ### compute gradients ###
             residuals = y_pred - y
@@ -39,8 +44,11 @@ class LogisticRegression:
             self.b -= self.learning_rate * grad_b
             self.losses.append(loss)
 
+            if (epoch + 1) % 100 == 0:
+                print(f"[Epoch {epoch + 1}/{self.epochs}] Loss: {round(loss, 5)}")
+
     def predict(self, X):
         z = np.matmul(X, self.W) + self.b
-        y_pred = self.sigmoid(z)
+        y_pred = self.__sigmoid(z)
         y_pred = np.where(y_pred >= self.threshold, 1, 0)
         return y_pred
