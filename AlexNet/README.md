0e6b8ac122115943acb56cd13724488df5cd9855
aandyw <andy999coc@gmail.com>
2024-08-26 02:26:25 -0400

    update

--- AlexNet/README.md
+++ AlexNet/README.md
@@ -10,3 +10,4 @@ A toy project to learn, implement, and train the famous AlexNet Architecture fro
 - [ImageNet Classification with Deep Convolutional Neural Networks](https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf)
 - [Writing AlexNet from Scratch in PyTorch](https://blog.paperspace.com/alexnet-pytorch/#data-loading)
 - https://pytorch.org/hub/pytorch_vision_alexnet/
+- [Difference between AlexNet, VGGNet, ResNet, and Inception](https://towardsdatascience.com/the-w3h-of-alexnet-vggnet-resnet-and-inception-7baaaecccc96)
--- AlexNet/trainer.py
+++ AlexNet/trainer.py
@@ -35,6 +35,7 @@ class Trainer:
         self.batch_size = batch_size  # does nothing; mainly for viz
         self.learning_rate = learning_rate
         self.momentum = momentum
+        self.weight_decay = weight_decay
         self.num_epochs = num_epochs
         self.check_val_every_n_epoch = check_val_every_n_epoch
         self.device = device
@@ -45,7 +46,7 @@ class Trainer:
 
         # SGD used by original alexnet paper
         self.optimizer = optim.SGD(
-            self.model.parameters(), lr=self.learning_rate, momentum=momentum, weight_decay=weight_decay
+            self.model.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=self.weight_decay
         )
 
         # Decays lr of each parameter group by 0.1 every step_size epochs
@@ -109,8 +110,9 @@ class Trainer:
             train_accuracy = running_acc / len(train_dataloader)
             avg_loss = running_loss / len(train_dataloader)
 
-            if avg_loss <= self.train_losses[-1]:
-                self.save(epoch, avg_loss)
+            prev_loss = self.train_losses[-1][0] if self.train_losses else float("inf")
+            if avg_loss <= prev_loss:
+                self.save(epoch + 1, avg_loss)
 
             self.train_accuracies.append((epoch, train_accuracy.cpu()))
             self.train_losses.append((epoch, avg_loss))
@@ -194,9 +196,8 @@ class Trainer:
     def _top_k_accuracy(self, outputs: torch.Tensor, labels: torch.Tensor, k: int) -> float:
         """Top-K accuracy. Top-1 is the equivalent to regular accuracy."""
 
-        ground_truths = torch.argmax(labels, dim=1)
         values, indices = torch.topk(outputs, k)
-        topk_correct = indices.eq(ground_truths.view(-1, 1).expand_as(indices))
+        topk_correct = indices.eq(labels.view(-1, 1).expand_as(indices))
         accuracy = topk_correct.sum().item() / labels.size(0)
 
         return accuracy
@@ -205,8 +206,10 @@ class Trainer:
         """Compute accuracy given outputs as logits"""
 
         preds = torch.argmax(outputs, dim=1)
-        return torch.sum(preds == labels) / len(preds)
+        accuracy = torch.sum(preds == labels) / len(preds)
 
+        return accuracy
+    
     def save(self, epoch: int, loss: float) -> None:
         """Save model"""
 
